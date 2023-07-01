7d69de3ad9c068cd53911fca492aaf6902fd47b2
dninosores <dninosores@gmail.com>
2023-07-01 18:45:44 -0400

    Made batch script for testing

---
+++ deploy.bat
@@ -0,0 +1,13 @@
+set NAME=separatebrusheraser
+
+del /S /Q %appdata%\krita\pykrita\%NAME%
+del /Q %appdata%\krita\pykrita\%NAME%.desktop
+del /Q %appdata%\krita\actions\%NAME%.action
+
+robocopy .\%NAME% %appdata%\krita\pykrita\%NAME% /E
+robocopy . %appdata%\krita\pykrita %NAME%.desktop
+robocopy .\%NAME% %appdata%\krita\actions %NAME%.action
+
+tar.exe -c -f .\%NAME%.zip .\%NAME% .\%NAME%.desktop
+
+"%PROGRAMFILES%\Krita (x64)\bin\krita.com"
