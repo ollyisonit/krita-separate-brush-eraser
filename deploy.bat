d65e203f01b30035e13bf1f6ac89ad3742a477af
dninosores <dninosores@gmail.com>
2023-07-01 19:33:46 -0400

    Fixed file zipping in deploy script

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
 set NAME=separatebrusheraser
+set ZIP="%PROGRAMFILES%\7-Zip\7z.exe"
+set KRITA="%PROGRAMFILES%\Krita (x64)\bin\krita.com"
 
 del /S /Q %appdata%\krita\pykrita\%NAME%
 del /Q %appdata%\krita\pykrita\%NAME%.desktop
@@ -9,6 +11,6 @@ robocopy .\%NAME% %appdata%\krita\pykrita\%NAME% /E
 robocopy . %appdata%\krita\pykrita %NAME%.desktop
 robocopy .\%NAME% %appdata%\krita\actions %NAME%.action
 
-tar.exe -c -f .\%NAME%.zip .\%NAME% .\%NAME%.desktop
+%ZIP% a -tzip .\%NAME%.zip .\%NAME% .\%NAME%.desktop
 
-"%PROGRAMFILES%\Krita (x64)\bin\krita.com"
+%KRITA%
